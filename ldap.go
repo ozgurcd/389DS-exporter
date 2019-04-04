b0de413cf9612bc5e2daeeb735bda2e33726ccfb
Ozgur Demir <odemir@snapfish-llc.com>
2019-04-03 22:39:34 -0700

    initial

---
+++ ldap.go
@@ -0,0 +1,332 @@
+// Ozgur
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/prometheus/common/log"
+	ldap "gopkg.in/ldap.v3"
+)
+
+// /usr/bin/ldapsearch -LLL -D "cn=Directory Manager" -w diz34RMD -h mldap1.op-zone1.phl1 -x -o ldif-wrap=no -o nettimeout=3 -b "cn=monitor" "(objectclass=*)"
+
+func getStats() DSData {
+	conn, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", "mldap1.op-zone1.phl1", 389))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	searchRequest := ldap.NewSearchRequest(
+		"cn=snmp, cn=monitor",
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		"(objectclass=*)",
+		nil,
+		nil,
+	)
+
+	sr, err := conn.Search(searchRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var anonymousbinds string
+	var unauthbinds string
+	var simpleauthbinds string
+	var strongauthbinds string
+	var bindsecurityerrors string
+	var inops string
+	var readops string
+	var compareops string
+	var addentryops string
+	var removeentryops string
+	var modifyentryops string
+	var modifyrdnops string
+	var searchops string
+	var onelevelsearchops string
+	var wholesubtreesearchops string
+	var referrals string
+	var securityerrors string
+	var errors string
+	var connections string
+	var connectionseq string
+	var connectionsinmaxthreads string
+	var connectionsmaxthreadscount string
+	var bytesrecv string
+	var bytessent string
+	var entriesreturned string
+	var referralsreturned string
+	var cacheentries string
+	var cachehits string
+
+	for _, entry := range sr.Entries {
+		attributes := entry.Attributes
+
+		for _, attr := range attributes {
+			name := string(attr.Name)
+			value := attr.Values
+
+			switch name {
+			case "anonymousbinds":
+				anonymousbinds = value[0]
+			case "unauthbinds":
+				unauthbinds = value[0]
+			case "simpleauthbinds":
+				simpleauthbinds = value[0]
+			case "strongauthbinds":
+				strongauthbinds = value[0]
+			case "bindsecurityerrors":
+				bindsecurityerrors = value[0]
+			case "inops":
+				inops = value[0]
+			case "readops":
+				readops = value[0]
+			case "compareops":
+				compareops = value[0]
+			case "addentryops":
+				addentryops = value[0]
+			case "removeentryops":
+				removeentryops = value[0]
+			case "modifyentryops":
+				modifyentryops = value[0]
+			case "modifyrdnops":
+				modifyrdnops = value[0]
+			case "searchops":
+				searchops = value[0]
+			case "onelevelsearchops":
+				onelevelsearchops = value[0]
+			case "wholesubtreesearchops":
+				wholesubtreesearchops = value[0]
+			case "referrals":
+				referrals = value[0]
+			case "securityerrors":
+				securityerrors = value[0]
+			case "errors":
+				errors = value[0]
+			case "connections":
+				connections = value[0]
+			case "connectionseq":
+				connectionseq = value[0]
+			case "connectionsinmaxthreads":
+				connectionsinmaxthreads = value[0]
+			case "connectionsmaxthreadscount":
+				connectionsmaxthreadscount = value[0]
+			case "bytesrecv":
+				bytesrecv = value[0]
+			case "bytessent":
+				bytessent = value[0]
+			case "entriesreturned":
+				entriesreturned = value[0]
+			case "referralsreturned":
+				referralsreturned = value[0]
+			case "cacheentries":
+				cacheentries = value[0]
+			case "cachehits":
+				cachehits = value[0]
+			default:
+				//fmt.Printf("Name: %s, Value: %s\n", name, value)
+			}
+		}
+	}
+
+	anonymousbinds64, err := strconv.ParseFloat(anonymousbinds, 64)
+	if err != nil {
+		log.Fatal(err)
+		anonymousbinds64 = 0
+	}
+
+	unauthbinds64, err := strconv.ParseFloat(unauthbinds, 64)
+	if err != nil {
+		log.Fatal(err)
+		unauthbinds64 = 0
+	}
+
+	simpleauthbinds64, err := strconv.ParseFloat(simpleauthbinds, 64)
+	if err != nil {
+		log.Fatal(err)
+		simpleauthbinds64 = 0
+	}
+
+	strongauthbinds64, err := strconv.ParseFloat(strongauthbinds, 64)
+	if err != nil {
+		log.Fatal(err)
+		strongauthbinds64 = 0
+	}
+
+	bindsecurityerrors64, err := strconv.ParseFloat(bindsecurityerrors, 64)
+	if err != nil {
+		log.Fatal(err)
+		bindsecurityerrors64 = 0
+	}
+
+	inops64, err := strconv.ParseFloat(inops, 64)
+	if err != nil {
+		log.Fatal(err)
+		inops64 = 0
+	}
+
+	readops64, err := strconv.ParseFloat(readops, 64)
+	if err != nil {
+		log.Fatal(err)
+		readops64 = 0
+	}
+
+	compareops64, err := strconv.ParseFloat(compareops, 64)
+	if err != nil {
+		log.Fatal(err)
+		compareops64 = 0
+	}
+
+	addentryops64, err := strconv.ParseFloat(addentryops, 64)
+	if err != nil {
+		log.Fatal(err)
+		addentryops64 = 0
+	}
+
+	removeentryops64, err := strconv.ParseFloat(removeentryops, 64)
+	if err != nil {
+		log.Fatal(err)
+		removeentryops64 = 0
+	}
+
+	modifyentryops64, err := strconv.ParseFloat(modifyentryops, 64)
+	if err != nil {
+		log.Fatal(err)
+		modifyentryops64 = 0
+	}
+
+	modifyrdnops64, err := strconv.ParseFloat(modifyrdnops, 64)
+	if err != nil {
+		log.Fatal(err)
+		modifyrdnops64 = 0
+	}
+
+	searchops64, err := strconv.ParseFloat(searchops, 64)
+	if err != nil {
+		log.Fatal(err)
+		searchops64 = 0
+	}
+
+	onelevelsearchops64, err := strconv.ParseFloat(onelevelsearchops, 64)
+	if err != nil {
+		log.Fatal(err)
+		onelevelsearchops64 = 0
+	}
+
+	wholesubtreesearchops64, err := strconv.ParseFloat(wholesubtreesearchops, 64)
+	if err != nil {
+		log.Fatal(err)
+		wholesubtreesearchops64 = 0
+	}
+
+	referrals64, err := strconv.ParseFloat(referrals, 64)
+	if err != nil {
+		log.Fatal(err)
+		referrals64 = 0
+	}
+
+	securityerrors64, err := strconv.ParseFloat(securityerrors, 64)
+	if err != nil {
+		log.Fatal(err)
+		securityerrors64 = 0
+	}
+
+	errors64, err := strconv.ParseFloat(errors, 64)
+	if err != nil {
+		log.Fatal(err)
+		errors64 = 0
+	}
+
+	connections64, err := strconv.ParseFloat(connections, 64)
+	if err != nil {
+		log.Fatal(err)
+		connections64 = 0
+	}
+
+	connectionseq64, err := strconv.ParseFloat(connectionseq, 64)
+	if err != nil {
+		log.Fatal(err)
+		connectionseq64 = 0
+	}
+
+	connectionsinmaxthreads64, err := strconv.ParseFloat(connectionsinmaxthreads, 64)
+	if err != nil {
+		log.Fatal(err)
+		connectionsinmaxthreads64 = 0
+	}
+
+	connectionsmaxthreadscount64, err := strconv.ParseFloat(connectionsmaxthreadscount, 64)
+	if err != nil {
+		log.Fatal(err)
+		connectionsmaxthreadscount64 = 0
+	}
+
+	bytesrecv64, err := strconv.ParseFloat(bytesrecv, 64)
+	if err != nil {
+		log.Fatal(err)
+		bytesrecv64 = 0
+	}
+
+	bytessent64, err := strconv.ParseFloat(bytessent, 64)
+	if err != nil {
+		log.Fatal(err)
+		bytessent64 = 0
+	}
+
+	entriesreturned64, err := strconv.ParseFloat(entriesreturned, 64)
+	if err != nil {
+		log.Fatal(err)
+		entriesreturned64 = 0
+	}
+
+	referralsreturned64, err := strconv.ParseFloat(referralsreturned, 64)
+	if err != nil {
+		log.Fatal(err)
+		referralsreturned64 = 0
+	}
+
+	cacheentries64, err := strconv.ParseFloat(cacheentries, 64)
+	if err != nil {
+		log.Fatal(err)
+		cacheentries64 = 0
+	}
+
+	cachehits64, err := strconv.ParseFloat(cachehits, 64)
+	if err != nil {
+		log.Fatal(err)
+		cachehits64 = 0
+	}
+
+	dsdata := DSData{
+		anonymousbinds64,
+		unauthbinds64,
+		simpleauthbinds64,
+		strongauthbinds64,
+		bindsecurityerrors64,
+		inops64,
+		readops64,
+		compareops64,
+		addentryops64,
+		removeentryops64,
+		modifyentryops64,
+		modifyrdnops64,
+		searchops64,
+		onelevelsearchops64,
+		wholesubtreesearchops64,
+		referrals64,
+		securityerrors64,
+		errors64,
+		connections64,
+		connectionseq64,
+		connectionsinmaxthreads64,
+		connectionsmaxthreadscount64,
+		bytesrecv64,
+		bytessent64,
+		entriesreturned64,
+		referralsreturned64,
+		cacheentries64,
+		cachehits64}
+
+	return dsdata
+}
---
+++ main.go
@@ -0,0 +1,381 @@
+// Ozgur Demir
+
+package main
+
+import (
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/version"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+const (
+	namespace = "ds_exporter"
+)
+
+// DSData stores metrics from 389DS
+type DSData struct {
+	anonymousbinds             float64
+	unauthbinds                float64
+	simpleauthbinds            float64
+	strongauthbinds            float64
+	bindsecurityerrors         float64
+	inops                      float64
+	readops                    float64
+	compareops                 float64
+	addentryops                float64
+	removeentryops             float64
+	modifyentryops             float64
+	modifyrdnops               float64
+	searchops                  float64
+	onelevelsearchops          float64
+	wholesubtreesearchops      float64
+	referrals                  float64
+	securityerrors             float64
+	errors                     float64
+	connections                float64
+	connectionseq              float64
+	connectionsinmaxthreads    float64
+	connectionsmaxthreadscount float64
+	bytesrecv                  float64
+	bytessent                  float64
+	entriesreturned            float64
+	referralsreturned          float64
+	cacheentries               float64
+	cachehits                  float64
+}
+
+// Exporter stores metrics from 389DS
+type Exporter struct {
+	anonymousbinds             *prometheus.Desc
+	unauthbinds                *prometheus.Desc
+	simpleauthbinds            *prometheus.Desc
+	strongauthbinds            *prometheus.Desc
+	bindsecurityerrors         *prometheus.Desc
+	inops                      *prometheus.Desc
+	readops                    *prometheus.Desc
+	compareops                 *prometheus.Desc
+	addentryops                *prometheus.Desc
+	removeentryops             *prometheus.Desc
+	modifyentryops             *prometheus.Desc
+	modifyrdnops               *prometheus.Desc
+	searchops                  *prometheus.Desc
+	onelevelsearchops          *prometheus.Desc
+	wholesubtreesearchops      *prometheus.Desc
+	referrals                  *prometheus.Desc
+	securityerrors             *prometheus.Desc
+	errors                     *prometheus.Desc
+	connections                *prometheus.Desc
+	connectionseq              *prometheus.Desc
+	connectionsinmaxthreads    *prometheus.Desc
+	connectionsmaxthreadscount *prometheus.Desc
+	bytesrecv                  *prometheus.Desc
+	bytessent                  *prometheus.Desc
+	entriesreturned            *prometheus.Desc
+	referralsreturned          *prometheus.Desc
+	cacheentries               *prometheus.Desc
+	cachehits                  *prometheus.Desc
+}
+
+// NewExporter returns an initialized exporter
+func NewExporter() *Exporter {
+	return &Exporter{
+
+		anonymousbinds: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "anonymousbinds"),
+			"Number of Anonymous Binds",
+			nil,
+			nil,
+		),
+
+		unauthbinds: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "unauthbinds"),
+			"Number of Unauth Binds",
+			nil,
+			nil,
+		),
+
+		simpleauthbinds: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "simpleauthbinds"),
+			"Number of Simple Auth Binds",
+			nil,
+			nil,
+		),
+
+		strongauthbinds: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "strongauthbinds"),
+			"Number of Strong Auth Binds",
+			nil,
+			nil,
+		),
+
+		bindsecurityerrors: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "bindsecurityerrors"),
+			"Number of Bind Security Errors",
+			nil,
+			nil,
+		),
+
+		inops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "inops"),
+			"Number of All Requests",
+			nil,
+			nil,
+		),
+
+		readops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "readops"),
+			"Number of Read Operations",
+			nil,
+			nil,
+		),
+
+		compareops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "compareops"),
+			"Number of Compare Operations",
+			nil,
+			nil,
+		),
+
+		addentryops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "addentryops"),
+			"Number of Add Entry Operations",
+			nil,
+			nil,
+		),
+
+		removeentryops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "removeentryops"),
+			"Number of Remove Entry Operations",
+			nil,
+			nil,
+		),
+
+		modifyentryops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "modifyentryops"),
+			"Number of Modify Entry Operations",
+			nil,
+			nil,
+		),
+
+		modifyrdnops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "modifyrdnops"),
+			"Number of Modify RDN Operations",
+			nil,
+			nil,
+		),
+
+		searchops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "searchops"),
+			"Number of LDAP Search Requests",
+			nil,
+			nil,
+		),
+
+		onelevelsearchops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "onelevelsearchops"),
+			"Number of one-level Search Requests",
+			nil,
+			nil,
+		),
+
+		wholesubtreesearchops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "wholesubtreesearchops"),
+			"Number of subtree-level Search Requests",
+			nil,
+			nil,
+		),
+
+		referrals: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "referrals"),
+			"Number of LDAP referrals",
+			nil,
+			nil,
+		),
+
+		securityerrors: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "securityerrors"),
+			"Number of Security Errors",
+			nil,
+			nil,
+		),
+
+		errors: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "errors"),
+			"Number of Errors",
+			nil,
+			nil,
+		),
+
+		connections: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "connections"),
+			"Number of Connections in Open State at the sampling time",
+			nil,
+			nil,
+		),
+
+		connectionseq: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "connectionseq"),
+			"Total Number of Connections opened",
+			nil,
+			nil,
+		),
+
+		connectionsinmaxthreads: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "connectionsinmaxthreads"),
+			"Number of connections that are currently in a max thread state",
+			nil,
+			nil,
+		),
+
+		connectionsmaxthreadscount: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "connectionsmaxthreadscount"),
+			"Number of connectionsmaxthreadscount",
+			nil,
+			nil,
+		),
+
+		bytesrecv: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "bytesrecv"),
+			"Total number of bytes received",
+			nil,
+			nil,
+		),
+
+		bytessent: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "bytessent"),
+			"Total number of bytes sent",
+			nil,
+			nil,
+		),
+
+		entriesreturned: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "entriesreturned"),
+			"Number of Entries Returned",
+			nil,
+			nil,
+		),
+
+		referralsreturned: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "referralsreturned"),
+			"Number of Referrals Returned",
+			nil,
+			nil,
+		),
+
+		cacheentries: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "cacheentries"),
+			"Number of Cache Entries",
+			nil,
+			nil,
+		),
+
+		cachehits: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "cachehits"),
+			"Number of Cache Hits",
+			nil,
+			nil,
+		),
+	}
+}
+
+// Describe soyle boyle
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+
+	ch <- e.anonymousbinds
+	ch <- e.unauthbinds
+	ch <- e.simpleauthbinds
+	ch <- e.strongauthbinds
+	ch <- e.bindsecurityerrors
+	ch <- e.inops
+	ch <- e.readops
+	ch <- e.compareops
+	ch <- e.addentryops
+	ch <- e.removeentryops
+	ch <- e.modifyentryops
+	ch <- e.modifyrdnops
+	ch <- e.searchops
+	ch <- e.onelevelsearchops
+	ch <- e.wholesubtreesearchops
+	ch <- e.referrals
+	ch <- e.securityerrors
+	ch <- e.errors
+	ch <- e.connections
+	ch <- e.connectionseq
+	ch <- e.connectionsinmaxthreads
+	ch <- e.connectionsmaxthreadscount
+	ch <- e.bytesrecv
+	ch <- e.bytessent
+	ch <- e.entriesreturned
+	ch <- e.referralsreturned
+	ch <- e.cacheentries
+	ch <- e.cachehits
+}
+
+// Collect reads stats from LDAP connection object into Prometheus objects
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	data := getStats()
+
+	ch <- prometheus.MustNewConstMetric(e.anonymousbinds, prometheus.CounterValue, data.anonymousbinds)
+	ch <- prometheus.MustNewConstMetric(e.unauthbinds, prometheus.CounterValue, data.unauthbinds)
+	ch <- prometheus.MustNewConstMetric(e.simpleauthbinds, prometheus.CounterValue, data.simpleauthbinds)
+	ch <- prometheus.MustNewConstMetric(e.strongauthbinds, prometheus.CounterValue, data.strongauthbinds)
+	ch <- prometheus.MustNewConstMetric(e.bindsecurityerrors, prometheus.CounterValue, data.bindsecurityerrors)
+	ch <- prometheus.MustNewConstMetric(e.inops, prometheus.CounterValue, data.inops)
+	ch <- prometheus.MustNewConstMetric(e.readops, prometheus.CounterValue, data.readops)
+	ch <- prometheus.MustNewConstMetric(e.compareops, prometheus.CounterValue, data.compareops)
+	ch <- prometheus.MustNewConstMetric(e.addentryops, prometheus.CounterValue, data.addentryops)
+	ch <- prometheus.MustNewConstMetric(e.removeentryops, prometheus.CounterValue, data.removeentryops)
+	ch <- prometheus.MustNewConstMetric(e.modifyentryops, prometheus.CounterValue, data.modifyentryops)
+	ch <- prometheus.MustNewConstMetric(e.modifyrdnops, prometheus.CounterValue, data.modifyrdnops)
+	ch <- prometheus.MustNewConstMetric(e.searchops, prometheus.CounterValue, data.searchops)
+	ch <- prometheus.MustNewConstMetric(e.onelevelsearchops, prometheus.CounterValue, data.onelevelsearchops)
+	ch <- prometheus.MustNewConstMetric(e.wholesubtreesearchops, prometheus.CounterValue, data.wholesubtreesearchops)
+	ch <- prometheus.MustNewConstMetric(e.referrals, prometheus.CounterValue, data.referrals)
+	ch <- prometheus.MustNewConstMetric(e.securityerrors, prometheus.CounterValue, data.securityerrors)
+	ch <- prometheus.MustNewConstMetric(e.errors, prometheus.CounterValue, data.errors)
+	ch <- prometheus.MustNewConstMetric(e.connections, prometheus.CounterValue, data.connections)
+	ch <- prometheus.MustNewConstMetric(e.connectionseq, prometheus.CounterValue, data.connectionseq)
+	ch <- prometheus.MustNewConstMetric(e.connectionsinmaxthreads, prometheus.CounterValue, data.connectionsinmaxthreads)
+	ch <- prometheus.MustNewConstMetric(e.connectionsmaxthreadscount, prometheus.CounterValue, data.connectionsmaxthreadscount)
+	ch <- prometheus.MustNewConstMetric(e.bytesrecv, prometheus.CounterValue, data.bytesrecv)
+	ch <- prometheus.MustNewConstMetric(e.bytessent, prometheus.CounterValue, data.bytessent)
+	ch <- prometheus.MustNewConstMetric(e.entriesreturned, prometheus.CounterValue, data.entriesreturned)
+	ch <- prometheus.MustNewConstMetric(e.referralsreturned, prometheus.CounterValue, data.referralsreturned)
+	ch <- prometheus.MustNewConstMetric(e.cacheentries, prometheus.CounterValue, data.cacheentries)
+	ch <- prometheus.MustNewConstMetric(e.cachehits, prometheus.CounterValue, data.cachehits)
+}
+
+func main() {
+	var (
+		listenAddress = kingpin.Flag("web.listen-address", "Address to listen on for web interface and telemetry.").Default(":9313").String()
+		metricsPath   = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
+	)
+
+	log.AddFlags(kingpin.CommandLine)
+	kingpin.Version(version.Print("ds_exporter"))
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	log.Infoln("Starting ds_exporter", version.Info())
+	log.Infoln("Build context", version.BuildContext())
+
+	prometheus.MustRegister(NewExporter())
+
+	http.Handle(*metricsPath, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+             <head><title>Memcached Exporter</title></head>
+             <body>
+             <h1>Memcached Exporter</h1>
+             <p><a href='` + *metricsPath + `'>Metrics</a></p>
+             </body>
+             </html>`))
+	})
+	log.Infoln("Starting HTTP server on", *listenAddress)
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
+}
